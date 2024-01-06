b0416602056c6b666d064f8458df7dc3bfa65d1b
annaannaA33 <129603016+annaannaA33@users.noreply.github.com>
2024-01-06 15:57:00 +0200

    Update FreeFormQuestion.py

--- FreeFormQuestion.py
+++ FreeFormQuestion.py
@@ -7,12 +7,9 @@ class FreeFormQuestion(Question):
         super().__init__(id, question_type, question_text, is_active)
         self.expected_answer = expected_answer
 
-    def update_statistics(self, is_correct):
-        super().update_statistics(is_correct)
-
 
     def as_dict(self):
-        # TODO apdate id
+        
         return  {'id': self.id, #присваиваем порядковый номер в общем списке в файле, начиная с первого, 
                  'question_type': self.question_type, 
                  'question_text': self.question_text,
@@ -20,6 +17,10 @@ class FreeFormQuestion(Question):
                  'is_active': self.get_is_active()
                  }
      
+    
+        
+    
+
     
         
     
