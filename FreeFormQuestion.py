883987c2cc4eeb2f1f99c84372cd0d1c9698807e
annaannaA33 <129603016+annaannaA33@users.noreply.github.com>
2024-01-08 12:14:07 +0200

    Update FreeFormQuestion.py

--- FreeFormQuestion.py
+++ FreeFormQuestion.py
@@ -3,8 +3,8 @@ from Question import Question
 
 
 class FreeFormQuestion(Question):
-    def __init__(self, id, question_type, question_text, expected_answer, is_active=True):
-        super().__init__(id, question_type, question_text, is_active)
+    def __init__(self, id, question_type, question_text, expected_answer, is_active=True, appearance_count=0, correct_count=0, total_correct_percentage=0):
+        super().__init__(id, question_type, question_text, is_active, appearance_count, correct_count, total_correct_percentage)
         self.expected_answer = expected_answer
 
 
@@ -14,13 +14,12 @@ class FreeFormQuestion(Question):
                  'question_type': self.question_type, 
                  'question_text': self.question_text,
                  'expected_answer': self.expected_answer,
-                 'is_active': self.get_is_active()
+                 'is_active': self.get_is_active(),
+                'appearance_count': self.appearance_count,
+                'correct_count': self.correct_count,
+                'total_correct_percentage': self.total_correct_percentage,
                  }
      
-    
-        
-    
-
     
         
     
