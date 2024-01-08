5ba5513b934c71e2676d65c63f78751ec67f225f
annaannaA33 <129603016+annaannaA33@users.noreply.github.com>
2024-01-08 12:13:44 +0200

    Update MultipleChoiceQuestion.py

--- MultipleChoiceQuestion.py
+++ MultipleChoiceQuestion.py
@@ -2,8 +2,8 @@ from Question import Question
 
 
 class MultipleChoiceQuestion(Question):
-    def __init__(self, id, question_type, question_text, options, correct_option, is_active=True):
-        super().__init__(id, question_type, question_text, options, is_active)
+    def __init__(self, id, question_type, question_text, options, correct_option, is_active=True, appearance_count=0, correct_count=0, total_correct_percentage=0):
+        super().__init__(id, question_type, question_text, is_active, appearance_count, correct_count, total_correct_percentage)
         self.correct_option = correct_option
         self.options = options
 #question_type, question_text, options, correct_option)
@@ -13,11 +13,14 @@ class MultipleChoiceQuestion(Question):
 
  
     def as_dict(self):
-        return  {'id': id,#присваиваем порядковый номер в общем списке в файле, начиная с первого, 
+        return  {'id': self.id,
                 'question_type': self.question_type, 
                 'question_text': self.question_text,
                 'options': self.options,
                 'correct_option': self.correct_option,
-                'is_active': self.get_is_active()
-                    }
+                'is_active': self.get_is_active(),
+                'appearance_count': self.appearance_count,
+                'correct_count': self.correct_count,
+                'total_correct_percentage': self.total_correct_percentage,
+                 }
     
