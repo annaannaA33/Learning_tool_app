fd46254557b76759cc0540e0166b575c12b1ce70
annaannaA33 <129603016+annaannaA33@users.noreply.github.com>
2024-01-12 03:13:56 +0200

    Update QuestionManager.py

--- QuestionManager.py
+++ QuestionManager.py
@@ -14,13 +14,13 @@ class QuestionManager:
     def create_free_form_question(self, question_type, question_text):
         # Ask for question_text and expected_answer, and if everything is ok
         # then save everything in the question_to_be_added list   
-        expected_answer = input("Enter the answer: ")
+        correct_answer = input("Enter the answer: ")
 
         new_question = FreeFormQuestion(
             id=id,
             question_type=question_type,
             question_text=question_text,
-            expected_answer=expected_answer
+            correct_answer=correct_answer
         )
         
         return new_question
@@ -38,14 +38,14 @@ class QuestionManager:
                 print("Invalid input. Please enter a valid number.")
 
         options = [input(f"Enter incorrect option {i+1}: ") for i in range(num_options - 1)]
-        correct_option = input("Now enter the correct option: ")
+        correct_answer = input("Now enter the correct option: ")
 
         new_question = MultipleChoiceQuestion(
             id=id,
             question_type=question_type,
             question_text=question_text,
             options=options,
-            correct_option=correct_option
+            correct_answer=correct_answer
         )
 
         print("You have prepared the question for saving.")
@@ -59,6 +59,8 @@ class QuestionManager:
 
         while True:
             print('You are in the question saving menu')
+            print("To add questions, follow the instructions. Questions with answer options and free-form questions are available for addition.")
+            print("To return to the main menu, type 'menu' or 'm'")
             question_type = input("Enter the question type (1 for FreeFormQuestion, 2 for MultipleChoiceQuestion): ")
             if question_type == '1' or question_type == '2':
                 question_text = input("Enter the question text: ")
@@ -73,7 +75,7 @@ class QuestionManager:
                         new_question = self.craete_multiple_choice_question(question_type, question_text)    
                         # When the question is saved, exit the loop, but not the function. The user is prompted to enter a question again until they exit
                     question_to_be_added.append(new_question)
-            elif question_type == "main_manu":
+            elif question_type.lower() == "menu" or question_type.lower() =='m':
                 if len(question_to_be_added) > 0:
                 # If at least one question was added to question_to_be_added, return question_to_be_added    
                     return question_to_be_added
