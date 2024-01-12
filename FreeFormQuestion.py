9d45d7f885cd9168f2f1d81253ed6ac6784bb6f9
annaannaA33 <129603016+annaannaA33@users.noreply.github.com>
2024-01-12 03:11:24 +0200

    Update FreeFormQuestion.py

--- FreeFormQuestion.py
+++ FreeFormQuestion.py
@@ -3,18 +3,19 @@ from Question import Question
 
 
 class FreeFormQuestion(Question):
-    def __init__(self, id, question_type, question_text, expected_answer, is_active=True, appearance_count=0, correct_count=0, total_correct_percentage=0):
-        super().__init__(id, question_type, question_text, is_active, appearance_count, correct_count, total_correct_percentage)
-        self.expected_answer = expected_answer
-
+    def __init__(self, id, question_type, question_text, correct_answer, is_active=True, appearance_count=0, correct_count=0, total_correct_percentage=0):
+        super().__init__(id, question_type, question_text, correct_answer, is_active, appearance_count, correct_count, total_correct_percentage=0)
+        
+    def get_correct_answer(self):
+        return self.correct_answer
 
     def as_dict(self):
         
-        return  {'id': self.id, #присваиваем порядковый номер в общем списке в файле, начиная с первого, 
-                 'question_type': self.question_type, 
-                 'question_text': self.question_text,
-                 'expected_answer': self.expected_answer,
-                 'is_active': self.get_is_active(),
+        return  {'id': self.id,
+                'question_type': self.question_type, 
+                'question_text': self.question_text,
+                'correct_answer': self.correct_answer,
+                'is_active': self.get_is_active(),
                 'appearance_count': self.appearance_count,
                 'correct_count': self.correct_count,
                 'total_correct_percentage': self.total_correct_percentage,
@@ -22,4 +23,6 @@ class FreeFormQuestion(Question):
      
     
         
+    
+
     
